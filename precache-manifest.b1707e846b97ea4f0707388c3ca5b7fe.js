c8ffcfe77c40b313606dc462dbff4726d6090c10
Vennverse <shubhamdubexskd2001@gmail.com>
2022-02-13 17:55:21 +0000

    Updates

---
+++ precache-manifest.b1707e846b97ea4f0707388c3ca5b7fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6f656332a705e5eac440",
+    "url": "/venn-tweet/static/css/main.6b8371ec.chunk.css"
+  },
+  {
+    "revision": "6f656332a705e5eac440",
+    "url": "/venn-tweet/static/js/main.6f656332.chunk.js"
+  },
+  {
+    "revision": "19385073ab7bdd27c0c8",
+    "url": "/venn-tweet/static/css/1.568227c8.chunk.css"
+  },
+  {
+    "revision": "19385073ab7bdd27c0c8",
+    "url": "/venn-tweet/static/js/1.19385073.chunk.js"
+  },
+  {
+    "revision": "425ea26a85d7296e51b9",
+    "url": "/venn-tweet/static/js/runtime~main.425ea26a.js"
+  },
+  {
+    "revision": "90890cb69700862a3c62c07b5c65df00",
+    "url": "/venn-tweet/index.html"
+  }
+];
