94b712e468340fde91babb6aac548ad6044abd20
Vennverse <shubhamdubexskd2001@gmail.com>
2022-02-14 11:27:51 +0000

    Updates

---
+++ precache-manifest.97683be67038c8e745e9e645c7c9334a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "07a6d1b33bebe6bdedf7",
+    "url": "/venn-tweet/static/css/main.fe7a9048.chunk.css"
+  },
+  {
+    "revision": "07a6d1b33bebe6bdedf7",
+    "url": "/venn-tweet/static/js/main.07a6d1b3.chunk.js"
+  },
+  {
+    "revision": "7dbd2c51414ad78f6573",
+    "url": "/venn-tweet/static/css/1.2a18771a.chunk.css"
+  },
+  {
+    "revision": "7dbd2c51414ad78f6573",
+    "url": "/venn-tweet/static/js/1.7dbd2c51.chunk.js"
+  },
+  {
+    "revision": "425ea26a85d7296e51b9",
+    "url": "/venn-tweet/static/js/runtime~main.425ea26a.js"
+  },
+  {
+    "revision": "3d14dc2fc1cece94f42c17c096a71c97",
+    "url": "/venn-tweet/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/meta/precache-manifest.cbf2c4630ac3a51e1feae60d603c318d.js"
+  "/venn-tweet/precache-manifest.97683be67038c8e745e9e645c7c9334a.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/meta/index.html", {
+workbox.routing.registerNavigationRoute("/venn-tweet/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
